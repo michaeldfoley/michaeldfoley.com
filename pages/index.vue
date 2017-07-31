212243438b89f045de9c28ea87175add1c3bb369
Michael Foley <michaeldfoley@gmail.com>
2017-07-31 17:12:27 -0400

    fixed container padding

--- pages/index.vue
+++ pages/index.vue
@@ -27,14 +27,6 @@ export default {
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 .title
 {
   display: block;
